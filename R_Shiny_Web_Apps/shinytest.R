98717bd49f89a6ba626d4a46b1d37b32022f536a
mikblack <blackmail@gmail.com>
2018-02-06 17:06:17 +1300

    Added shinytest.

--- R_Shiny_Web_Apps/shinytest.R
+++ R_Shiny_Web_Apps/shinytest.R
@@ -1,4 +1,13 @@
+## Install required packages.
 ## devtools::install_github("rstudio/shinytest")
+## webdriver::install_phantomjs()
+
+## Load shinytest package
 library(shinytest)
 
+## Record a test for this application
+recordTest('carsApp')
 
+## Test the current behaviour of the application to how it behaved
+## at baseline.
+testApp("carsApp", "carsAppTest.R")
